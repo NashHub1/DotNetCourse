e83cf59f6a6e64e7cb215d92bdcf925845104310
Michael Stephens <stephensnash@gmx.de>
2024-07-22 19:59:02 +0200

    brauche ich eig nicht

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc794908978e096b57bfc0f42797f18efa7e1bc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebd0bbb7d9daa106a5705b7ad0c28b4211ab1cf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
