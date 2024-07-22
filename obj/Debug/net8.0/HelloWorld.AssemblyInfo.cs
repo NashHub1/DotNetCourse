2b47db91173d440a20e4f7b18723bd6827a312d9
Michael Stephens <stephensnash@gmx.de>
2024-07-22 20:02:51 +0200

    Neuster stand ?

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83cf59f6a6e64e7cb215d92bdcf925845104310")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6f39ce891b8a27ffa56936f8f20824125be58e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
