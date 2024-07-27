33801274a800af650ea19d5e83426279c333e107
Michael Stephens <stephensnash@gmx.de>
2024-07-27 17:51:27 +0200

    JSON Property and Automapper fuctions.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Dynamic;
 using System.Globalization;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using AutoMapper;
 using Dapper;
 using HelloWorld;
 using HelloWorld.Data;
@@ -30,43 +31,48 @@ namespace HelloWorld
             DataContextDapper dapper = new DataContextDapper(config);
 
             // Read the content of 'Computers.json' into a string variable to display in the console.
-            string computersJson = File.ReadAllText("Computers.json");
-            // // Output the content of the log file to the console.
-            // Console.WriteLine(computersJson);
-
-
-            // Deserialize JSON string to a collection of Computer objects using Newtonsoft.Json.
-            // Newtonsoft.Json (Json.NET) has been the de facto standard for JSON processing in .NET
-            // before System.Text.Json was introduced. It handles camelCase by default, 
-            // requiring less configuration and resulting in simpler code.
-            IEnumerable<Computer>? computersNewtonSoft = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
-
-            //Checks if the deserialization was successful and the collection is not null.
-            if (computersNewtonSoft != null)
+            string computersJson = File.ReadAllText("ComputersSnake.json");
+            
+            Mapper mapper = new Mapper(new MapperConfiguration((cfg) => {
+                cfg.CreateMap<ComputerSnake, Computer>()
+                    .ForMember(destination => destination.ComputerId, options => 
+                        options.MapFrom(source => source.computer_id))
+                    .ForMember(destination => destination.CPUCores, options => 
+                        options.MapFrom(source => source.cpu_cores))
+                    .ForMember(destination => destination.HasLTE, options => 
+                        options.MapFrom(source => source.has_lte))
+                    .ForMember(destination => destination.HasWifi, options => 
+                        options.MapFrom(source => source.has_wifi))
+                    .ForMember(destination => destination.Motherboard, options => 
+                        options.MapFrom(source => source.motherboard))
+                    .ForMember(destination => destination.VideoCard, options => 
+                        options.MapFrom(source => source.video_card))
+                    .ForMember(destination => destination.ReleaseDate, options => 
+                        options.MapFrom(source => source.release_date))
+                    .ForMember(destination => destination.Price, options => 
+                        options.MapFrom(source => source.price));
+            }));
+            
+            IEnumerable<ComputerSnake>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<ComputerSnake>>(computersJson);
+            if (computersSystem != null)
             {
-                // Iterates over each Computer object in the deserialized collection.
-                foreach (Computer computer in computersNewtonSoft)
-                {
-                    // Create an SQL insert command to add the computer data to the database
-                    string sql = @"INSERT INTO TutorialAppSchema.Computer (
-                        Motherboard,
-                        HasWifi,
-                        HasLTE,
-                        ReleaseDate,
-                        Price,
-                        VideoCard
-                    ) VALUES ('" + EscapeSingleQuote(computer.Motherboard)
-                            + "','" + computer.HasWifi
-                            + "','" + computer.HasLTE
-                            + "','" + computer.ReleaseDate
-                            + "','" + computer.Price.ToString("0.00", CultureInfo.InvariantCulture)
-                            + "','" + EscapeSingleQuote(computer.VideoCard)
-                    + "')";
-                    // Execute the SQL command using the Dapper context
-                    dapper.ExecuteSql(sql);
-                }
-            }          
+                IEnumerable<Computer> computerResult = mapper.Map<IEnumerable<Computer>>(computersSystem);
+                Console.WriteLine("Automapper Count: " +  computerResult.Count());
+                // foreach (Computer computer in computerResult)
+                // {
+                //     Console.WriteLine(computer.Motherboard);
+                // }
+            }
 
+            IEnumerable<Computer>? computersJsonPropertyMapping = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson);
+            if (computersJsonPropertyMapping != null)
+            {
+                Console.WriteLine("JSON Property Count: " + computersJsonPropertyMapping.Count());
+                // foreach (Computer computer in computersJsonPropertyMapping)
+                // {
+                //     Console.WriteLine(computer.Motherboard);
+                // }
+            }            
 
         }
         // Method to escape single quotes in a string to prevent SQL injection
@@ -77,10 +83,6 @@ namespace HelloWorld
             return output;
         }
     }
-
-
-
-
 }
 
 
