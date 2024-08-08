60e980097c91ff88f122f9ec7ecc62056f264896
Michael Stephens <stephensnash@gmx.de>
2024-08-08 20:19:37 +0200

    Test

--- Program.cs
+++ Program.cs
@@ -76,6 +76,7 @@ namespace HelloWorld
 
         }
         // Method to escape single quotes in a string to prevent SQL injection
+        //Test
         static string EscapeSingleQuote(string input)
         {
             string output = input.Replace("'", "''");
